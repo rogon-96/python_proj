9d43f5ca5981d9e1fadaec16cf25db9de9cdcff4
rogon-96 <motukurumidhunreddy@gmail.com>
2021-11-28 12:03:31 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,url_for,request,redirect,flash
 import csv,os
 from flask_mail import Mail, Message
+from werkzeug.utils import secure_filename
+# from func import *
 app = Flask(__name__)
-mail = Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'YOUR_GMAIL_ID'
@@ -10,38 +11,93 @@ app.config['MAIL_PASSWORD'] = 'YOUR_GMAIL_PASSWORD'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-@app.route('/',methods=['GET','POST'])
-def index():
-    return render_template('index.html')
+bool_dict = {
+    'master_response': '',
+    'responses_response': ''
+}
+sample_input_path = "./sample_input"
+sample_output_path = "./sample_output"
 
-@app.route('/data',methods=['GET','POST'])
-def data():
-    if request.method == "POST":
-        # print(request.form)
-        # data = "hello"
-        data = []
-        master =csv.reader(request.files.get('master').read().decode('utf-8').splitlines())
-        response = csv.reader(request.files.get('response').read().decode('utf-8').splitlines())
-        for row in master:
-            data.append(list(row))
-        return render_template("data.html",data=data)
+
+def handle_file_save(FileObject,req_file_name,req_resp_name):
+    if not FileObject.filename:
+        bool_dict[f"{req_resp_name}"] = "Didn't upload any file."
+        return   
+    file_name = FileObject.filename
+    if file_name!=req_file_name:
+        bool_dict[f"{req_resp_name}"] = "Uploaded a wrong file..plz Upload {}".format(req_file_name)
+        return 
+    sample_input= os.path.join(os.getcwd(),"sample_input")
+
+    os.makedirs(sample_input,exist_ok = True)
+
+    if os.path.exists(f"./sample_input/{file_name}"):
+        os.remove(f"./sample_input/{file_name}")
+
+    filename = secure_filename(file_name)
+    FileObject.save(os.path.join(sample_input_path,filename))
+    
+    bool_dict[f"{req_resp_name}"] = "Uploaded Successfully"
+    return 
+
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html',data = bool_dict)
+
+@app.route('/master',methods=['GET','POST'])
+def master():
+    if request.method=="POST":
+        FileObject = request.files.get("master")
+        handle_file_save(FileObject,"master_roll.csv","master_response")
+    return redirect(url_for('index'))
+
+@app.route('/response',methods=['GET','POST'])
+def response():
+    print(os.getcwd())
+    FileObject = request.files.get("response")
+    handle_file_save(FileObject,"responses.csv","responses_response")
+    return redirect(url_for('index')) 
+
+
+@app.route('/RollNo',methods=['GET','POST'])
+def rollno():
+    bool_dict['positive_response'] = bool_dict['negative_response']=''
+    if not os.path.exists(os.path.join(sample_input_path,"master_roll.csv")):
+        bool_dict["rollno_response"] = "You have not Uploaded master_roll.csv"
+        return redirect(url_for('index'))
+    if not os.path.exists(os.path.join(sample_input_path,"responses.csv")):
+        bool_dict["rollno_response"] = "You have not Uploaded responses.csv"
+        return redirect(url_for('index'))
+    if request.form.get('positive')=='':
+        bool_dict["positive_response"] = "This field is required"
+        return redirect(url_for('index'))
+    if request.form.get('negative')=='':
+        bool_dict["negative_response"] = "This field is required"
+        return redirect(url_for('index'))
+    #Python Code must be inserted here
+    bool_dict["rollno_response"] = "Generated Successfully"
+    return redirect(url_for('index'))
+
+
+@app.route('/concise',methods=['GET','POST'])
+def concise():
+    #check if the files are existed then
+    #Python Code must be inserted here
+    bool_dict["concise_response"] = "Generated Successfully"
+    return redirect(url_for('index'))
+
 @app.route('/sendemail',methods=['GET','POST'])
 def send_email():
     files = os.listdir('sample_output\marksheet')
-    # print(files)
-    # msg = Message('Hello',sender ='motukurumidhun@gmail.com',recipients = ['midhunreddymotukuru@gmail.com','venkat.abhilashreddy@gmail.com'])
-    # msg.body = 'Hello Flask message sent from Flask-Mail'
-    # with app.open_resource('./sample_output/marksheet/1401CB01.xlsx') as fp:  
-    #     msg.attach('1401CB01.xlsx', "application/xlsx", fp.read()) 
-    #     print(f"Success mail sent '1401CB01.xlsx'")
-    #     mail.send(msg)
-    for file in files :
+    for file in files:
         msg = Message('Hello',sender ='motukurumidhun@gmail.com',recipients = ['venkat.abhilashreddy@gmail.com'])
         msg.body = 'Hello Flask message sent from Flask-Mail'
         with app.open_resource(f"./sample_output/marksheet/{file}") as fp:  
             msg.attach(f"{file}", "application/xlsx", fp.read()) 
             mail.send(msg)
             print(f"Success mail sent{file}")
-    return 'Sent Email successfully'   
+    bool_dict["email_response"] = "Email Sent Successfully"
+    return redirect(url_for('index')) 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ proj1.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import os 
+import openpyxl
+
+slist = pd.read_csv('./sample_input/master_roll.csv')
+response = pd.read_csv('./sample_input/responses.csv')
+sample_output_path = "./sample_output"
+
+def get_answer():
+    for index,row in response.iterrows():
+        if row['Roll Number'] == 'ANSWER':
+            return [key for key in row][7:]
+    return []
+
+def generate_concise():
+    crt = get_answer()
+    if crt == []:
+        return "Error!!!Answer is not exist in responses"
+    lst,score_after_neg,= [0,0,0],[]
+    
+    return  
+   
+def generateMarksheet(mrks,wmrks):
+    crt = get_answer(response)
+    if crt == []:
+        return "Error!!!Answer is not exist in responses"
+    os.makedirs(sample_output_path,exist_ok = True)
+    for i in range(len(response)):       
+        wb =  openpyxl.Workbook() #creation and initialising a workbook
+        sheet = wb.active
+        sheet.title = response.at[i,'Roll Number']
+        sheet.merge_cells('A1:E4')
+        # add image here
+        sheet.merge_cells('A5:E5')
+        sheet.cell(row=5,column=1).value = "Mark Sheet"
+        sheet.cell(row=6,column=1).value = "Name:"
+        sheet.merge_cells('B6:C6')
+        sheet.cell(row=6,column=2).value = slist.at[i,'name']
+        sheet.cell(row=6,column=4).value = "Exam:"
+        sheet.cell(row=6,column=5).value = "quiz"
+        sheet.cell(row=7,column=1).value = "Roll Number:"
+        sheet.cell(row=7,column=2).value = response.at[i,'Roll Number']
+
+        sheet.cell(row=9,column=2).value = "Right"
+        sheet.cell(row=9,column=3).value = "Wrong"
+        sheet.cell(row=9,column=4).value = "Not Attempt"
+        sheet.cell(row=9,column=5).value = "Max"
+        sheet.cell(row=10,column=1).value = "No."
+        sheet.cell(row=11,column=1).value = "Marking"
+        sheet.cell(row=12,column=1).value = "Total"
+
+        r,c = 16,1
+        rgt,mrks,wrg,wmrks,na = 0,5,0,-1,0
+        sheet.cell(row=15,column=1).value = "Student Ans"
+        sheet.cell(row=15,column=2).value = "Correct Ans"
+        sheet.cell(row=15,column=4).value = "Student Ans"
+        sheet.cell(row=15,column=5).value = "Correct Ans"
+
+        ## create empty files when they are not submitted
+        lt = []
+        for j in range(7,35) : #Checking the Options
+            if(j==32): r,c=16,4
+            sheet.cell(row=r,column=c).value = response.iat[i,j]
+            sheet.cell(row=r,column=c+1).value = crt[j-7]
+            if(response.iat[i,j]==crt[j-7]):#counting right,wrong and not attempted answers 
+                rgt+=1
+            else: wrg+=1
+            r+=1
+            lt.append(response.iat[i,j])
+            # print(response.iat[i,j])
+
+        na = pd.Series(lt).isna().sum()
+        wrg-=na
+        sheet.cell(row=10,column=2).value = rgt
+        sheet.cell(row=11,column=2).value = mrks
+        sheet.cell(row=12,column=2).value = rgt*mrks
+        sheet.cell(row=10,column=3).value = wrg
+        sheet.cell(row=11,column=3).value = wmrks
+        sheet.cell(row=12,column=3).value = wrg*wmrks
+        sheet.cell(row=10,column=4).value = na
+        sheet.cell(row=11,column=4).value = 0
+        sheet.cell(row=10,column=5).value = rgt+wrg+na
+        sheet.cell(row=12,column=5).value = str(rgt*mrks+wrg*wmrks)+'/'+str(140)
+        wb.save("./sample_output/"+sheet.title+'.xlsx')
+    return
+# generateMarksheet(5,-1)
+generate_concise()
